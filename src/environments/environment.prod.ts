0e7402b1ff48cd97540c34897752a12c1d9646a4
emmy omega <omegaemmy@gmail.com>
2019-10-23 00:22:14 +0300

    adds firebase to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBrqK8IpTKFWQmJwyOP988GlPnEMAuOSsc',
+    authDomain: 'mws-p2c1.firebaseapp.com',
+    databaseURL: 'https://mws-p2c1.firebaseio.com',
+    projectId: 'mws-p2c1',
+    storageBucket: 'mws-p2c1.appspot.com',
+    messagingSenderId: '199716533892',
+    appId: '1:199716533892:web:109343126dc1ba7647f4d1',
+    measurementId: 'G-6W8ZZR19FT'
+  }
 };
